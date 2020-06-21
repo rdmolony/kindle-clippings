66e514b0e0f98962b555b1dcee5b2af7d93298ed
Rowan Molony <rowanmolony@gmail.com>
2020-06-21 16:32:27 +0100

    Save clippings in defaultdict
    Using the book title as keys...

--- kindle.py
+++ kindle.py
@@ -9,8 +9,11 @@ from collections import defaultdict
 from sys import platform
 from shutil import copyfile
 from pathlib import Path
+from typing import List
 
+import prefect
 from prefect import Flow, task
+from prefect.utilities.debug import raise_on_exception
 from pipeop import pipes
 
 BOUNDARY = u"==========\r\n"
@@ -18,19 +21,10 @@ DATA_FILE = u"clips.json"
 OUTPUT_DIR = u"output"
 MY_CLIPPINGS = Path(u'My Clippings.txt')
 
-@pipes
-def _etl_clippings_to_folder():
 
-    with Flow("Extract clippings from Kindle to files") as flow:
+# Tasks
+# *****
 
-        path_to_clippings = _get_path_to_kindle_clippings.run()
-        _copy_clippings_from_kindle_to_cwd(path_to_clippings)
-
-        _load_clippings_to_string() >> _split_clippings_into_ordereddict
-
-
-
-@task
 def _get_path_to_kindle_clippings() -> Path:
 
     if platform == 'darwin': # Mac / OSX 
@@ -49,18 +43,29 @@ def _get_path_to_kindle_clippings() -> Path:
     return path_to_clippings
 
 
-@task
 def _copy_clippings_from_kindle_to_cwd(clippings_local: Path, clippings_kindle: Path) -> None:
 
-    if not clippings_local.exists():
-        if clippings_kindle.exists():
-            copyfile(clippings_kindle, clippings_local)
-        else:
-            raise Exception(
-            "Cannot find Kindle!\n"
-            f"Ensure Kindle is plugged in and 'My Clippings.txt' is saved at {clippings_kindle}"
-        )
+    if clippings_kindle.exists():
+        copyfile(clippings_kindle, clippings_local)
+    else:
+        raise Exception(
+        "Cannot find Kindle!\n"
+        f"Ensure Kindle is plugged in and 'My Clippings.txt' is saved at {clippings_kindle}"
+    )
 
+
+@task
+def _extract_kindle_clippings(clippings_local: Path = Path(u"My Clippings.txt")) -> Path:
+
+    logger = prefect.context.get("logger")
+    
+    if not clippings_local.exists():
+        kindle_clippings = _get_path_to_kindle_clippings()
+        _copy_clippings_from_kindle_to_cwd(clippings_local, clippings_kindle)
+    else:
+        logger.info("Clippings already exists locally.  Delete local copy to extract again...")
+        
+    return clippings_local
 
 @task
 def _load_clippings_to_string(filename: Path):
@@ -71,28 +76,85 @@ def _load_clippings_to_string(filename: Path):
     return content
 
 
-@task
-def _split_clippings_into_ordereddict(raw_clippings: str):
+def _split_clippings_string_into_list(raw_clippings: str) -> List[str]:
 
-    books_with_quotes = defaultdict(list)
-    clippings = (
+    return (
         raw_clippings.replace(u"\r", "")
         .replace(u"\n\n", "\n")
         .replace(u"\ufeff", "")
         .split("==========")
     )
 
+
+def _link_books_with_quotes(clippings: List[str]) -> defaultdict(list):
+
+    books_with_quotes = defaultdict(list)
+
     for clipping in clippings:
         split_clipping = clipping.strip(u"\n").split(u"\n")
+
         book = split_clipping[0]
-        quote = split_clipping[-1]
-        books_with_quotes[book].append(quote)
-    
+
+        # NOTE: don't save empty quotes (i.e. len==2)
+        if len(split_clipping) == 3: 
+            quote = split_clipping[2]
+
+            books_with_quotes[book].append(quote)
+
+    import ipdb; ipdb.set_trace()
     return books_with_quotes
 
 
+def _link_books_with_quotes_and_location(clippings: List[str]) -> defaultdict(list):
+
+    books_with_quotes_and_location = defaultdict(list) 
+
+    location_regex = re.compile(r"(location \d+-\d+)")
+
+    for clipping in clippings:
+        split_clipping = clipping.strip(u"\n").split(u"\n")
+    
+        book = split_clipping[0]
+
+        # NOTE: don't save empty quotes (i.e. len==2)
+        if len(split_clipping) == 3: 
+            location = location_regex.findall(split_clipping[1])[0]
+            quote = split_clipping[2]
+
+            books_with_quotes_and_location[book].append([quote, location])
+
+    return books_with_quotes_and_location
+
+
 @task
-def _
+def _transform_clippings_into_ordereddict(raw_clippings: str):
+
+    clippings = _split_clippings_string_into_list(raw_clippings)
+
+    return _link_books_with_quotes(clippings)
+
+
+# Flow
+# ****
+
+@pipes
+def _etl_clippings_to_folder():
+
+    with Flow("Extract clippings from Kindle to files") as flow:
+
+        clippings_local = _extract_kindle_clippings()
+        clippings_by_book = (
+            _load_clippings_to_string(clippings_local) 
+            >> _transform_clippings_into_ordereddict 
+        )
+
+        # _save_clippings_to_files(clippings_by_book)
+
+    return flow
+
+
+# @task
+# def _
 
 def export_txt(clips):
     """
@@ -156,4 +218,9 @@ def main():
 
 
 if __name__ == '__main__':
-    main()
+    # main()
+    flow = _etl_clippings_to_folder()
+
+    with raise_on_exception():
+        flow.run()
+
